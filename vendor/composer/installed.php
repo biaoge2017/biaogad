cff0adacc0e93f0fe5b8366d0df1005458d0f289
dabiaoge <649883823@qq.com>
2022-03-02 18:36:54 +0800

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3e3cb0ca8411eea1e5b38babb66170e2255cccee',
+        'name' => 'hidogad/hidogad',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hidogad/hidogad' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3e3cb0ca8411eea1e5b38babb66170e2255cccee',
+            'dev_requirement' => false,
+        ),
+    ),
+);
