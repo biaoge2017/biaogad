d4fa77f8452e6851b6fb7670e3de8ddc6d186cc8
dabiaoge <649883823@qq.com>
2022-03-03 09:43:22 +0800

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '16a4fdc2ca25f4c829442034052ece086966c4a7',
+        'name' => 'hidog/biaogad',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hidog/biaogad' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '16a4fdc2ca25f4c829442034052ece086966c4a7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
